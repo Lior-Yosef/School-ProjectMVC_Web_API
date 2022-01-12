129046013b3cc42240cf3ad350dfb094d0b0d218
Lior-Yosef <lioryosef06@gmail.com>
2022-01-12 13:42:24 +0200

    first Commit

---
+++ School-ProjectMVC_Web_API/Controllers/StudentController.cs
@@ -0,0 +1,51 @@
+﻿using School_ProjectMVC_Web_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace School_ProjectMVC_Web_API.Controllers
+{
+    public class StudentController : Controller
+    {
+        Student[] students = new Student[]
+        {
+            new Student(11,"lior","yosef","A",12),
+            new Student(12,"eilay","yosef","b",13),
+            new Student(13,"avi","yosef","c",14)
+
+        };
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View(students[0]);
+            
+        }
+
+        public ActionResult StudentName()
+        {
+            return View(students[0]);
+
+        }
+        public ActionResult GetAll()
+        {
+            return View(students);
+        }
+        public ActionResult StudentById(int id)
+        {
+           foreach (Student student in students)
+            {
+                if(student.Id == id)
+                {
+                    return View(student);
+                }
+            }
+
+            return View("not fuond");
+
+        }
+
+
+    }
+}
--- School-ProjectMVC_Web_API/Models/Student.cs
+++ School-ProjectMVC_Web_API/Models/Student.cs
@@ -12,6 +12,15 @@ namespace School_ProjectMVC_Web_API.Models
         public string LastName;
         public string Grade;
         public int Age;
-        public 
+
+        public Student(int id, string firstName, string lastName, string grade, int age)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Grade = grade;
+            this.Age = age;
+        }
+        
     }
 }
